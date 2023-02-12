ddbb96da52f6147c09f2dae9ae6eecc3d76e5916
Arman <arman_m121@yahoo.com>
2023-02-12 15:18:17 +0330

    add MessagesScreen into ChatStack

--- NilaSoftProject/src/navigation/ChatStack.tsx
+++ NilaSoftProject/src/navigation/ChatStack.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { ChatScreen } from '~/screens';
+import { ChatScreen, MessagesScreen } from '~/screens';
 import CustomHeader from '~/components/atom/CustomeHeader';
 
 export type ChatStackParamList = { ChatScreen: undefined };
@@ -10,14 +10,22 @@ export default function ChatStack() {
     return (
         <>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name={'ChatScreen'} component={ChatScreen}
+                <Stack.Screen name={'MessagesScreen'} component={MessagesScreen}
                     options={{
-                        headerTitle: 'صفحه چت',
+                        headerTitle: 'لیست گفتکو ها',
                         headerShown: true,
                         header: ({ route, options, navigation }: any) => (
                             <CustomHeader back {...{ route, options, navigation }} />
                         ),
                     }} />
+                <Stack.Screen name={'ChatScreen'} component={ChatScreen}
+                    options={{
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }} />
+
             </Stack.Navigator>
         </>
     );
