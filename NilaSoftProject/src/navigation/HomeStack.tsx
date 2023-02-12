c6f1e5091acbeb0699e9d32ad311dc70e2121a47
Arman <arman_m121@yahoo.com>
2023-02-12 12:16:14 +0330

    implement HomeStack

---
+++ NilaSoftProject/src/navigation/HomeStack.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from '../screens';
+export type MainStackParamList = { HomeScreen: undefined };
+
+export default function HomeStack() {
+    const Stack = createNativeStackNavigator();
+    return (
+        <>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name={'HomeScreen'} component={HomeScreen} />
+            </Stack.Navigator>
+        </>
+    );
+}
