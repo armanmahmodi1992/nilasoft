2d9f30b0675b5427b53a0a661b9878bafe1435c2
Arman <arman_m121@yahoo.com>
2023-02-12 13:27:06 +0330

    add CustomHeader

--- NilaSoftProject/src/navigation/HomeStack.tsx
+++ NilaSoftProject/src/navigation/HomeStack.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from '../screens';
+import CustomHeader from '../components/atom/CustomeHeader';
+
 export type HomeStackParamList = { HomeScreen: undefined };
 
 export default function HomeStack() {
@@ -8,7 +10,14 @@ export default function HomeStack() {
     return (
         <>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name={'HomeScreen'} component={HomeScreen} />
+                <Stack.Screen name={'HomeScreen'} component={HomeScreen}
+                    options={{
+                        headerTitle: 'صفحه اصلی',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader {...{ route, options, navigation }} />
+                        ),
+                    }} />
             </Stack.Navigator>
         </>
     );
