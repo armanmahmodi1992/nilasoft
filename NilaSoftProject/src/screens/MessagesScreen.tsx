a5148a056bd0136678d9abee56b6e06d5ee87255
Arman <arman_m121@yahoo.com>
2023-02-12 23:12:24 +0330

    implement MessageScreen

--- NilaSoftProject/src/screens/MessagesScreen.tsx
+++ NilaSoftProject/src/screens/MessagesScreen.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, FlatList, StyleSheet, Text } from 'react-native'
+import { Divider } from 'native-base';
 import React from 'react'
+import { items } from '~/store/MessageData';
+import MessageCart from '~/components/atom/MessageCart';
+import { colors } from '~/styles';
+
+const renderItem = ({ item }: { item: any }) => {
+    return (
+        <MessageCart items={item} />
+    )
+}
+
+const itemSeparator = () => (
+    <Divider m='2' />
+)
 
 export default function MessagesScreen() {
     return (
-        <View>
-            <Text>MessagesScreen</Text>
+        <View style={styles.container}>
+            <FlatList
+                contentContainerStyle={styles.contentContainerStyle}
+                data={items}
+                keyExtractor={(_, index) => `itm${index}`}
+                renderItem={renderItem}
+                ItemSeparatorComponent={itemSeparator}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    contentContainerStyle: {
+        flexGrow: 1,
+        marginHorizontal: 4,
+        backgroundColor: colors.GARY_4
+    }
+})
