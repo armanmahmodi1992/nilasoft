1875332c8923471be8cb3a6732699719674995bb
Arman <arman_m121@yahoo.com>
2023-02-12 11:41:04 +0330

    add style

--- NilaSoftProject/src/screens/HomeScreen.tsx
+++ NilaSoftProject/src/screens/HomeScreen.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
 export default function HomeScreen() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>صفحه اصلی</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
