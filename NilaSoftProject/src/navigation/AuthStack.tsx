80c271452adeaaec139653e5bcbcf77e46a76df5
Arman <arman_m121@yahoo.com>
2023-02-12 12:21:47 +0330

    implement AuthStack

---
+++ NilaSoftProject/src/navigation/AuthStack.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ProfileScreen } from '../screens';
+export type MainStackParamList = { ProfileScreen: undefined };
+
+export default function AuthStack() {
+    const Stack = createNativeStackNavigator();
+    return (
+        <>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name={'ProfileScreen'} component={ProfileScreen} />
+            </Stack.Navigator>
+        </>
+    );
+}
