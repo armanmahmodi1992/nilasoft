f8d0bb755c5b2616d4fe09d61ee8f060291b81ed
Arman <arman_m121@yahoo.com>
2023-02-12 14:45:34 +0330

    implement MessagesScreen

--- NilaSoftProject/src/screens/index.ts
+++ NilaSoftProject/src/screens/index.ts
@@ -2,3 +2,4 @@ export {default as HomeScreen} from './HomeScreen';
 export {default as ChatScreen} from './ChatScreen';
 export {default as ProfileScreen} from './ProfileScreen';
 export {default as SplashScreen} from './SplashScreen';
+export {default as MessagesScreen} from './MessagesScreen';
