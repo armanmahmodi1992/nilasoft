7fc16a2b8c57b3ddd93296bddb7267605c838a1e
Arman <arman_m121@yahoo.com>
2023-02-13 11:37:01 +0330

    add new image

--- NilaSoftProject/src/assets/images/index.ts
+++ NilaSoftProject/src/assets/images/index.ts
@@ -2,10 +2,12 @@ import avatar1 from './1.jpg';
 import avatar2 from './2.png';
 import avatar3 from './3.jpg';
 import avatar4 from './4.png';
+import rain from './rain.jpg';
 
 export default {
     avatar1,
     avatar2,
     avatar3,
-    avatar4
+    avatar4,
+    rain
 }
