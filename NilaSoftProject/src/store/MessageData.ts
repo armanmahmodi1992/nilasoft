e9619fd065c7dab7c448f993f39ab927a1f6b02f
Arman <arman_m121@yahoo.com>
2023-02-13 18:50:20 +0330

    change MessageData

--- NilaSoftProject/src/store/MessageData.ts
+++ NilaSoftProject/src/store/MessageData.ts
@@ -1,7 +1,7 @@
 import images from '~/assets/images';
 
-export const items = [{ id: 1, conversationId:1, name: 'کامران', avatar:images.avatar1 },
-{ id: 1,conversationId:2, name: 'آرمان',avatar:images.avatar2},
-{ id: 2,conversationId:3, name: 'جلال',avatar:images.avatar3 },
-{ id: 3,conversationId:4, name: 'جعفر',avatar:images.avatar4 },
+export const items = [{ id: 1, conversationId:1, name: 'Kamran', avatar:images.avatar1 },
+{ id: 1,conversationId:2, name: 'Arman',avatar:images.avatar2},
+{ id: 2,conversationId:3, name: 'Jalal',avatar:images.avatar3 },
+{ id: 3,conversationId:4, name: 'Jafar',avatar:images.avatar4 },
 ]
