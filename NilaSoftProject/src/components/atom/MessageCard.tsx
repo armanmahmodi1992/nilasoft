fcd66ad2eef700b593293e84973f624f8a818f1d
Arman <arman_m121@yahoo.com>
2023-02-13 11:46:16 +0330

    rename MessageCard

--- NilaSoftProject/src/components/atom/MessageCard.tsx
+++ NilaSoftProject/src/components/atom/MessageCard.tsx
@@ -2,19 +2,17 @@ import { HStack, Text, View } from 'native-base';
 import React from 'react';
 import { StyleSheet, TouchableOpacity, Image } from 'react-native';
 import CustomImage from '~/components/atom/CustomImage';
+import { navigate } from '~/navigation/Methods';
 import { colors } from '~/styles';
 
-export default function CartCard({ items }: { items: any }) {
-    console.log({ items })
-
+export default function MessageCard({ items }: { items: any }) {
 
     return (
         <View style={styles.content}>
-            <TouchableOpacity onPress={() => { }}>
+            <TouchableOpacity onPress={() => navigate('ChatScreen', items)}>
                 <HStack h='60' w='100%' direction='row-reverse' alignItems='center' borderWidth='1' borderRadius='10' p='2' borderColor={colors.GARY_4} >
                     <Text style={styles.description}>{items?.name}</Text>
-                    {/* <CustomImage style={styles.image} imageSource={items?.avatar} resizeMode='cover' /> */}
-                    <Image source={require('../../assets/images/1.jpg')} style={styles.image} />
+                    <CustomImage local style={styles.image} imageSource={items?.avatar} resizeMode='cover' />
                 </HStack>
             </TouchableOpacity>
         </View>
@@ -40,7 +38,8 @@ const styles = StyleSheet.create({
         height: 60,
         borderRadius: 30,
         borderWidth: 1,
-        borderColor: colors.GARY_4
+        borderColor: colors.GARY_4,
+        overflow: 'hidden'
     },
 
 });
