1b0b0dbb71de8204ce82ebcb34202b4662fd0db4
Arman <arman_m121@yahoo.com>
2023-02-13 18:41:42 +0330

    Add ChatStack into MainStack

--- NilaSoftProject/src/navigation/MainStack.tsx
+++ NilaSoftProject/src/navigation/MainStack.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from '~/navigation/TabNavigator';
+import { ChatScreen } from '~/screens';
 
-export type MainStackParamList = { TabNavigator: undefined };
+export type MainStackParamList = { TabNavigator: undefined, Chat: { conversationId: number; userId: number } };
 
 export default function MainStackNavigator() {
     const Stack = createNativeStackNavigator();
@@ -10,6 +11,8 @@ export default function MainStackNavigator() {
         <>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name={'TabNavigator'} component={TabNavigator} />
+                <Stack.Screen name={'Chat'} component={ChatScreen}
+                    options={{ headerShown: false }} />
             </Stack.Navigator>
         </>
     );
