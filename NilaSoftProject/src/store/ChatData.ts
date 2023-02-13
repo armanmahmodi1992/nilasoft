18a02c8ddcf545d5eef1f65926184dd378e49fac
Arman <arman_m121@yahoo.com>
2023-02-13 18:43:17 +0330

    implement ChatData

---
+++ NilaSoftProject/src/store/ChatData.ts
@@ -0,0 +1,9 @@
+import  images  from '~/assets/images';
+export const items = [
+    {  conversationId:1,userId:1 ,message:'hello',imageUrl:'' },
+    {  conversationId:1,userId:2 ,message:'hello',imageUrl:'' },
+    {  conversationId:1,userId:1 ,message:'how are you?',imageUrl:'' },
+    {  conversationId:1,userId: 2,message: 'i am fine',imageUrl:''},
+    {  conversationId:1,userId:1 ,message:'what is it like out?',imageUrl:'' },
+    {  conversationId:1,userId:2 ,message:'raining heavily' ,imageUrl:images.rain},
+]
