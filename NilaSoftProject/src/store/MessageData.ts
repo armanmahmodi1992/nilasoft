37edc83d4cf0aaad0a888ce84a9ddc77feca7b45
Arman <arman_m121@yahoo.com>
2023-02-13 11:14:03 +0330

    change avatar address

--- NilaSoftProject/src/store/MessageData.ts
+++ NilaSoftProject/src/store/MessageData.ts
@@ -1,5 +1,7 @@
-export const items = [{ id: 1, conversationId:1, name: 'کامران', avatar:'../assets/images/1.jpg' },
-{ id: 1,conversationId:2, name: 'آرمان',avatar:'../assets/images/2.png' },
-{ id: 2,conversationId:3, name: 'جلال',avatar:'../assets/images/3.jpg' },
-{ id: 3,conversationId:4, name: 'جعفر',avatar:'../assets/images/4.png' },
+import images from '~/assets/images';
+
+export const items = [{ id: 1, conversationId:1, name: 'کامران', avatar:images.avatar1 },
+{ id: 1,conversationId:2, name: 'آرمان',avatar:images.avatar2},
+{ id: 2,conversationId:3, name: 'جلال',avatar:images.avatar3 },
+{ id: 3,conversationId:4, name: 'جعفر',avatar:images.avatar4 },
 ]
