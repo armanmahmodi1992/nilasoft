60dd164018b9e642786d875033a1da05e1a1ab8c
Arman <arman_m121@yahoo.com>
2023-02-13 11:07:25 +0330

    implement index for images

---
+++ NilaSoftProject/src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import avatar1 from './1.jpg';
+import avatar2 from './2.png';
+import avatar3 from './3.jpg';
+import avatar4 from './4.png';
+
+export default {
+    avatar1,
+    avatar2,
+    avatar3,
+    avatar4
+}
